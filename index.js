e2acb309cacd001749a7943306857260f884d75c
RensGits <somzoastudios@gmail.com>
2021-05-02 10:48:57 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+    //iterates over grid items and assigns something to it
+    
+    for (let blockNumber = 1; blockNumber <= 100; blockNumber++ ){
+        console.log(blockNumber)
+        blockName = 'block' + blockNumber
+        console.log(blockName);
+        document.getElementById(blockName).innerHTML = 'test'
+    
+    }
+
+  
+
+
+  
+
+
+
+
